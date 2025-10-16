406b1f2a6a1f707c256c69140a25337531d9ffa2
Thang <thangpham03111999@gmail.com>
2025-10-16 23:19:28 +0700

    thêm env và config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
@@ -24,7 +24,7 @@ export default function Home() {
             Save and see your changes instantly.
           </li>
         </ol>
-<Button variant="outline">Button</Button> 
+        <Button variant="outline">Button</Button>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
